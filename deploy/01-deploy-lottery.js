11a31fcc328ce856f793b82c659fcf1fc750b60d
kushan2k <kushangayantha001@gmail.com>
2023-09-04 12:25:36 +0530

    simple bug fixes

--- deploy/01-deploy-lottery.js
+++ deploy/01-deploy-lottery.js
@@ -1,7 +1,7 @@
 const { network, ethers } = require("hardhat")
 const { isDevelopmentChain, chainData } = require("../help.conf")
 
-const FUMDING_AMOUNT = ethers.parseEther("20")
+const FUMDING_AMOUNT = ethers.parseEther("1")
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
@@ -16,7 +16,8 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     // console.log(subidTX.value)
     const subREcipt = await subidTX.wait(1)
 
-    subid = subREcipt.logs[0]
+    subid = subREcipt.events[0].args.subid
+    console.log(subid)
 
     // await VRFMock.fundSubscription(subid, FUMDING_AMOUNT)
   } else {
@@ -27,10 +28,17 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   keyhash = chainData[network.config.chainId]["keyHash"]
   gassLimit = chainData[network.config.chainId]["callbackgaslimit"]
 
-  const Lottery = await deploy("Lottery", {
-    from: deployer,
-    log: true,
-    args: [vrfAddress, keyhash, subid, gassLimit],
-  })
+  // const Lottery = await deploy("Lottery", {
+  //   from: deployer,
+  //   log: true,
+  //   args: [vrfAddress, keyhash, subid, gassLimit],
+  // })
+
+  // if (isDevelopmentChain(network.name)) {
+  //   const vrfCoordinatorV2Mock = await ethers.getContract(
+  //     "VRFCoordinatorV2Mock"
+  //   )
+  //   await vrfCoordinatorV2Mock.addConsumer(subid, Lottery.address)
+  // }
 }
 module.exports.tags = ["all", "lottery"]
